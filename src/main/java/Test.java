1302b947fee7edbe88b88ec70a45e7447ae394b9
burning0117 <zhemuyouqing@gmail.com>
2016-03-15 23:08:31 +0800

    1212

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by burning on 2016/3/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("java");
+        System.out.println("hhh");
+    }
+}
