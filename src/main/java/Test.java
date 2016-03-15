fa9d06de971dfc5f63b5a43cc10e8e0b9fcced89
burning0117 <zhemuyouqing@gmail.com>
2016-03-15 23:12:39 +0800

    1212

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.printf("java");
         System.out.println("hhh");
+        System.out.printf("dakfj");
     }
 }
